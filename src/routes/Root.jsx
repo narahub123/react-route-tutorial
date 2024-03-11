a797e91a25c0842adad863dfaeeabc226186b4a3
nara <parknara0213@gmail.com>
2024-03-12 05:14:59 +0900

    240312 synchronize urls to form state

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import {
   Outlet,
   NavLink,
@@ -19,12 +20,16 @@ export async function loader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 } // loader() ends
 
 const Root = () => {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
+
   return (
     <>
       <div id="sidebar">
@@ -37,6 +42,7 @@ const Root = () => {
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
