29157b32e9d3145eeee423856ff1c356a253f4f5
nara <parknara0213@gmail.com>
2024-03-12 05:38:09 +0900

    240312 manage the history stack

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -52,7 +52,10 @@ const Root = () => {
               name="q"
               defaultValue={q}
               onChange={(e) => {
-                submit(e.currentTarget.form);
+                const isFirstSearch = q === null;
+                submit(e.currentTarget.form, {
+                  replace: !isFirstSearch,
+                });
               }}
             />
             <div id="search-spinner" aria-hidden hidden={!searching} />
